e6ff78abad336e45da86a4347d414c087aaffece
yiiqii <yfj5tzl2005@sina.com>
2024-07-25 12:05:40 +0800

    build: 优化 rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
 
 const plugins = [
   typescript(),
@@ -9,9 +10,12 @@ const plugins = [
     ignoreGlobal: true,
   }),
 ];
+const platform = 'alipay';
 
 export default () => {
   const input = {};
+  const dependencies = Object.keys(pkg.dependencies);
+  const manualChunks = [];
 
   [
     'index',
@@ -26,6 +30,14 @@ export default () => {
     input[name] = `./miniprogram/pages/${name}/index.ts`;
   });
 
+  dependencies.forEach(name => {
+    const reg = /^(?:@[^/]+\/)?([^/]+)/i;
+    const key = name.match(reg)[1];
+    const value = [name.indexOf('@galacean') === 0 ? `${name}/${platform}` : name];
+
+    manualChunks[key] = value;
+  });
+
   return {
     input,
     output: {
@@ -33,12 +45,7 @@ export default () => {
       dir: 'miniprogram/',
       entryFileNames: 'pages/[name]/index.js',
       chunkFileNames: 'chunks/[name].js',
-      manualChunks: {
-        'appx-adapter': ['@galacean/appx-adapter/alipay'],
-        'effects': ['@galacean/effects/alipay'],
-        'plugin-spine': ['@galacean/effects-plugin-spine/alipay'],
-        'plugin-model': ['@galacean/effects-plugin-model/alipay'],
-      },
+      manualChunks,
     },
     plugins,
   }
