2a6f2e8d19bac7512ff3841ee7de47822ae24179
yiiqii <yfj5tzl2005@sina.com>
2024-08-26 15:26:31 +0800

    chore: update libs

--- pages/index/index.ts
+++ pages/index/index.ts
@@ -10,16 +10,17 @@ Page<{
   },
   async onLoad (query) {
     // 页面加载
+  },
+  async onReady () {
+    // 页面加载完成
     const canvas = await adapter.registerCanvas({ id: '#J-webglCanvas' });
-
     // 实例化一个播放器
+    // @ts-expect-error
     const player = new Player({
       canvas,
     });
-    void player.loadScene('https://gw.alipayobjects.com/os/gltf-asset/mars-cli/MNJVBYCSYDWN/1425978492-8d707.json');
-  },
-  onReady () {
-    // 页面加载完成
+
+    await player.loadScene('https://gw.alipayobjects.com/os/gltf-asset/mars-cli/MNJVBYCSYDWN/1425978492-8d707.json');
   },
   onShow () {
     // 页面显示
