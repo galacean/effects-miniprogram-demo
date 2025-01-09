8eff02bc2f3fd041a13a0920994b079ea7e8a509
yiiqii <yfj5tzl2005@sina.com>
2025-01-09 15:47:56 +0800

    build: add terser for mini, update effects

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
 
 const plugins = [
   typescript(),
@@ -8,6 +9,7 @@ const plugins = [
   commonjs({
     ignoreGlobal: true,
   }),
+  terser(),
 ];
 
 export default [
