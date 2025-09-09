36298f787e1d18bec058686758535509af747fbe
慕还 <zhengjiang.jz@antgroup.com>
2025-09-09 10:39:11 +0800

    feat: add interactive demo

--- pages/interactive/index.vue
+++ pages/interactive/index.vue
@@ -1,31 +1,49 @@
 <template>
-	<view>
-		
-	</view>
+<view style="width: 100vw; height: 100vh;">
+	<canvas type="webgl"
+			id="J-webglCanvas"
+			style="width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.6);"
+			disable-scroll="{{ true }}"
+			@touchcancel="touchCancel" 
+			@touchend="touchEnd" 
+			@touchmove="touchMove" 
+			@touchstart="touchStart">
+	</canvas>
+</view>
 </template>
 
 <script setup lang="ts">
 import { onMounted, onBeforeUnmount } from 'vue';
-import { registerCanvas } from '@galacean/appx-adapter/weapp';
+import { registerCanvas, dispatchTouchStart, dispatchTouchMove, dispatchTouchEnd, dispatchTouchCancel } from '@galacean/appx-adapter/weapp';
 import { Player } from '@galacean/effects/weapp';
 
-const json = 'https://mdn.alipayobjects.com/mars/afts/file/A*PubBSpHUbjYAAAAAAAAAAAAADlB4AQ';
+const json = 'https://mdn.alipayobjects.com/mars/afts/file/A*cyyfTavfBscAAAAAAAAAAAAADlB4AQ';
 
 let player: Player = null;
-		
+
+function touchCancel (e: any) {
+	dispatchTouchCancel(e);
+}
+function touchStart (e: any) {
+    dispatchTouchStart(e);
+}
+function touchMove (e: any) {
+    dispatchTouchMove(e);
+}
+function touchEnd (e: any) {
+    dispatchTouchEnd(e);
+}
+
 onMounted(async () => {
 	try {
 		const canvas = await registerCanvas({ id: '#J-webglCanvas' });
 	
 		player = new Player({
 			canvas,
+			interactive: true,
 			renderFramework: 'webgl',
 		});
-		await player.loadScene(json, {
-			variables: {
-			  image: 'https://mdn.alipayobjects.com/huamei_klifp9/afts/img/A*ySrfRJvfvfQAAAAAAAAAAAAADvV6AQ/.png',
-			},
-		});
+		await player.loadScene(json);
 	} catch (e) {
 		console.error(`biz error: ${e}`);
 	}
